84c0e47d8e89b813460fa04ae7b7bdf3179c1dbf
lauraoliveira-r <102702720+lauraoliveira-r@users.noreply.github.com>
2025-06-12 12:08:53 -0300

    Create ReservaService.java

---
+++ ReservaService.java
@@ -0,0 +1,11 @@
+package reserva;
+
+public class ReservaService {
+    public String reservar(String cidade, String endereco) {
+        if (cidade.equalsIgnoreCase("Belo Horizonte") || cidade.equalsIgnoreCase("Contagem")) {
+            return "Motorista a caminho";
+        } else {
+            return "Área fora de cobertura";
+        }
+    }
+}
